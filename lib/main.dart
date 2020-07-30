fda58f8f272e50223d19861e8517b3e9886b7ebe
senaybee <senay_berihu@outlook.com>
2020-07-30 16:07:11 -0700

    Atomically Increment Votes

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,8 @@ Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
       child: ListTile(
         title: Text(record.name),
         trailing: Text(record.votes.toString()),
-        onTap: () => print(record),
+        onTap: () =>
+            record.reference.updateData({'votes': FieldValue.increment(1)}),
       ),
     ),
   );
